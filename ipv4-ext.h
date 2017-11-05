3aac8aff9a318c5723fb6b45f1eb9d7a67c43a09
Alexey Pechnikov <pechnikov@mobigroup.ru>
2017-11-05 19:47:04 +0700

    Move code from my own fossil repository

---
+++ ipv4-ext.h
@@ -0,0 +1,11 @@
+#include "sqlite3.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif  /* __cplusplus */
+
+int sqlite3InetInit(sqlite3 *db);
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif  /* __cplusplus */
